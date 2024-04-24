80926c749ed4a6fd67df01ac024394fefbcc0235
Islam Aliev <aliev.islam@gmail.com>
2024-04-24 10:19:44 +0200

    Make IN handle optional values

--- connor/src/op_in.rs
+++ connor/src/op_in.rs
@@ -1,29 +1,65 @@
 pub fn handle(condition: &core::doc::Field, doc_field: &core::doc::Field) -> bool {
     match condition {
-        core::doc::Field::IntArray(int_arr_cond) => {
-            if let core::doc::Field::Int(int_val) = doc_field {
-                return int_arr_cond.contains(&int_val);
+        core::doc::Field::BoolArray(arr_cond) => {
+            if let core::doc::Field::Bool(val) = doc_field {
+                return arr_cond.contains(&val);
             }
             false
         }
-        core::doc::Field::FloatArray(float_arr_cond) => {
-            if let core::doc::Field::Float(float_val) = doc_field {
-                return float_arr_cond.contains(&float_val);
+        core::doc::Field::IntArray(arr_cond) => {
+            if let core::doc::Field::Int(val) = doc_field {
+                return arr_cond.contains(&val);
             }
             false
         }
-        core::doc::Field::StringArray(str_arr_cond) => {
-            if let core::doc::Field::String(str_val) = doc_field {
-                return str_arr_cond.contains(&str_val);
+        core::doc::Field::FloatArray(arr_cond) => {
+            if let core::doc::Field::Float(val) = doc_field {
+                return arr_cond.contains(&val);
             }
             false
         }
-        core::doc::Field::DateTimeArray(date_time_arr_cond) => {
-            if let core::doc::Field::DateTime(date_time_val) = doc_field {
-                return date_time_arr_cond.contains(&date_time_val);
+        core::doc::Field::StringArray(arr_cond) => {
+            if let core::doc::Field::String(val) = doc_field {
+                return arr_cond.contains(&val);
             }
             false
         }
+        core::doc::Field::DateTimeArray(arr_cond) => {
+            if let core::doc::Field::DateTime(val) = doc_field {
+                return arr_cond.contains(&val);
+            }
+            false
+        }
+        core::doc::Field::OptionalBoolArray(arr_cond) => match doc_field {
+            core::doc::Field::OptionalBool(val) => arr_cond.contains(&val),
+            core::doc::Field::Bool(val) => arr_cond.contains(&Some(*val)),
+            core::doc::Field::Null => arr_cond.contains(&None),
+            _ => false,
+        },
+        core::doc::Field::OptionalIntArray(arr_cond) => match doc_field {
+            core::doc::Field::OptionalInt(val) => arr_cond.contains(&val),
+            core::doc::Field::Int(val) => arr_cond.contains(&Some(*val)),
+            core::doc::Field::Null => arr_cond.contains(&None),
+            _ => false,
+        },
+        core::doc::Field::OptionalFloatArray(arr_cond) => match doc_field {
+            core::doc::Field::OptionalFloat(val) => arr_cond.contains(&val),
+            core::doc::Field::Float(val) => arr_cond.contains(&Some(*val)),
+            core::doc::Field::Null => arr_cond.contains(&None),
+            _ => false,
+        },
+        core::doc::Field::OptionalStringArray(arr_cond) => match doc_field {
+            core::doc::Field::OptionalString(val) => arr_cond.contains(&val),
+            core::doc::Field::String(val) => arr_cond.contains(&Some(val.clone())),
+            core::doc::Field::Null => arr_cond.contains(&None),
+            _ => false,
+        },
+        core::doc::Field::OptionalDateTimeArray(arr_cond) => match doc_field {
+            core::doc::Field::OptionalDateTime(val) => arr_cond.contains(&val),
+            core::doc::Field::DateTime(val) => arr_cond.contains(&Some(val.clone())),
+            core::doc::Field::Null => arr_cond.contains(&None),
+            _ => false,
+        },
         _ => return false,
     }
 }
--- connor/src/tests.rs
+++ connor/src/tests.rs
@@ -536,6 +536,13 @@ fn test_le_null() {
 }
 
 #[test]
+fn test_in_bool() {
+    let cond = Condition::Op(Op::IN, Field::BoolArray(vec![true]));
+    expect(match_conditions(&cond, &Field::Bool(true)), true, false);
+    expect(match_conditions(&cond, &Field::Bool(false)), false, false);
+}
+
+#[test]
 fn test_in_int() {
     let cond = Condition::Op(Op::IN, Field::IntArray(vec![5, 6, 7]));
     expect(match_conditions(&cond, &Field::Int(6)), true, false);
@@ -579,6 +586,70 @@ fn test_in_time() {
 
     let tomorrow = Field::DateTime(now_time() + Duration::days(1));
     expect(match_conditions(&cond, &tomorrow), false, false);
+}
+
+#[test]
+fn test_in_optional_bool() {
+    let cond = Condition::Op(Op::IN, Field::OptionalBoolArray(vec![Some(true), None]));
+    expect(match_conditions(&cond, &Field::Bool(true)), true, false);
+    expect(match_conditions(&cond, &Field::Null), true, false);
+    expect(match_conditions(&cond, &Field::Bool(false)), false, false);
+}
+
+#[test]
+fn test_in_optional_int() {
+    let cond = Condition::Op(
+        Op::IN,
+        Field::OptionalIntArray(vec![Some(5), None, Some(6)]),
+    );
+    expect(match_conditions(&cond, &Field::Int(6)), true, false);
+    expect(match_conditions(&cond, &Field::Null), true, false);
+    expect(match_conditions(&cond, &Field::Int(4)), false, false);
+}
+
+#[test]
+fn test_in_optional_float() {
+    let cond = Condition::Op(
+        Op::IN,
+        Field::OptionalFloatArray(vec![Some(5.0), None, Some(6.0)]),
+    );
+    expect(match_conditions(&cond, &Field::Float(6.0)), true, false);
+    expect(match_conditions(&cond, &Field::Null), true, false);
+    expect(match_conditions(&cond, &Field::Float(4.0)), false, false);
+}
+
+#[test]
+fn test_in_optional_str() {
+    let cond = Condition::Op(
+        Op::IN,
+        Field::OptionalStringArray(vec![
+            Some("str1".to_string()),
+            None,
+            Some("str2".to_string()),
+        ]),
+    );
+    expect(
+        match_conditions(&cond, &Field::String("str2".to_string())),
+        true,
+        false,
+    );
+    expect(match_conditions(&cond, &Field::Null), true, false);
+    expect(
+        match_conditions(&cond, &Field::String("str4".to_string())),
+        false,
+        false,
+    );
+}
+
+#[test]
+fn test_in_optional_time() {
+    let cond = Condition::Op(
+        Op::IN,
+        Field::OptionalDateTimeArray(vec![Some(now_time()), None]),
+    );
+    expect(match_conditions(&cond, &now()), true, false);
+    expect(match_conditions(&cond, &Field::Null), true, false);
+    expect(match_conditions(&cond, &yesterday()), false, false);
 }
 
 #[test]
